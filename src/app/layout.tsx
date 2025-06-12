08b6f6de7a3296a3d70668efeba6af464d4cca10
hunghztr <tranhunghat141512@gmail.com>
2025-06-12 13:27:13 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
-
+"use client"
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import "./globals.css";
+
+// Tạo theme tùy chỉnh (nếu muốn)
+const theme = createTheme({
+  palette: {
+    mode: "light", // hoặc "dark"
+  },
+});
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,7 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
